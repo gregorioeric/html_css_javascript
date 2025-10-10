bc55c28b3765b0cafdcf9b4ec10866d992d0e181
gregorioeric <gregorio.eric@gmail.com>
2025-10-10 11:20:58 -0300

    Novo projeto javascript

--- 01_aula_funcoes_e_eventos/assets/js/app.js
+++ 01_aula_funcoes_e_eventos/assets/js/app.js
@@ -11,9 +11,18 @@ focusin, focusout, keyup,
 keydown
 */
 
+function mostrar_mensagem(valorInput) {
+  const msg = `
+    Nome do Cliente é: ${valorInput}
+  `;
+
+  return (resultado.innerHTML = msg);
+}
+
 btnEnviar.addEventListener("click", (e) => {
   e.preventDefault();
   const campo_input_valor = campo_input.value;
   // innerHTML => Coloca informação no html
-  resultado.innerHTML = campo_input_valor;
+  // resultado.innerHTML = campo_input_valor;
+  mostrar_mensagem(campo_input_valor);
 });
