d7551d7696486cb5f30d630aec9def1752cee7fa
gregorioeric <gregorio.eric@gmail.com>
2025-10-10 10:35:51 -0300

    Criando funções e eventos com javascript

--- 01_aula_funcoes_e_eventos/assets/js/app.js
+++ 01_aula_funcoes_e_eventos/assets/js/app.js
@@ -1,1 +1,19 @@
+const campo_input = document.querySelector("#campo-input");
+const form_simples = document.querySelector("#form-simples");
+const btnEnviar = document.querySelector("#btn-enviar");
+const resultado = document.querySelector("#resultado");
 
+/*
+Eventos:
+
+click, focus, input, change,
+focusin, focusout, keyup,
+keydown
+*/
+
+btnEnviar.addEventListener("click", (e) => {
+  e.preventDefault();
+  const campo_input_valor = campo_input.value;
+  // innerHTML => Coloca informação no html
+  resultado.innerHTML = campo_input_valor;
+});
