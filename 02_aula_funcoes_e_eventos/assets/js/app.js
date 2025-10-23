a17b0e357f3e953ce092154c1a6a046a51a2784f
gregorioeric <gregorio.eric@gmail.com>
2025-10-23 13:01:09 -0300

    Novo codigo para criar novas constantes e criar funções de evento

--- 02_aula_funcoes_e_eventos/assets/js/app.js
+++ 02_aula_funcoes_e_eventos/assets/js/app.js
@@ -3,9 +3,22 @@ const email = document.querySelector("#email");
 const telefone = document.querySelector("#telefone");
 const nascimento = document.querySelector("#nascimento");
 const btn_salvar = document.querySelector("#btn-salvar");
+const resultado = document.querySelector("#resultado");
+
+const db_dados = localStorage.getItem("Clientes")
+  ? JSON.parse(localStorage.getItem("Clientes"))
+  : [];
 
 btn_salvar.addEventListener("click", (e) => {
   e.preventDefault();
+
+  if (!nome.value || !email.value || !telefone.value || !nascimento.value) {
+    return console.log("Campos não podem ser vazios!");
+  }
+
+  if (nome.value.length < 3) {
+    return console.log("Nome precisa ter no minimo 3 caracteres");
+  }
 
   const dados = {
     nome: nome.value,
@@ -14,15 +27,31 @@ btn_salvar.addEventListener("click", (e) => {
     nascimento: nascimento.value,
   };
 
-  if (!nome.value || !email.value || !telefone.value || !nascimento.value) {
-    return console.log("Campos não podem ser vazios!");
-  }
+  db_dados.push(dados);
+  localStorage.setItem("Clientes", JSON.stringify(db_dados));
 
-  console.log(dados);
+  nome.value = "";
+  email.value = "";
+  telefone.value = "";
+  nascimento.value = "";
 });
 
-nome.addEventListener("input", () => {});
+nome.addEventListener("input", () => {
+  if (nome.value.length < 3) {
+    return console.log("Nome precisa ter no minimo 3 caracteres");
+  }
+});
 
 email.addEventListener("input", () => {});
 
 telefone.addEventListener("input", () => {});
+
+function mostrar_dados() {
+  const get_dados = db_dados.map((usuario) => {
+    return `
+      <div>${usuario.nome}</div>
+    `;
+  });
+
+  resultado.innerHTML = get_dados;
+}
